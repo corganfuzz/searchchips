285aac0730636ffef95e75bfab6bb0127e259209
corganfuzz <gruizcavero@uh.edu>
2017-08-30 18:59:49 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import './App.css';
+import Searching from './Searching';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <Searching />
+      </div>
+    );
+  }
+}
+
+export default App;
